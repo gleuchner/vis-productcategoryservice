77c6901ac3d2c7a71e0020b1ad25c1a7da7264b9
Micha <Micha.Kroeker@gmail.com>
2017-05-04 14:44:30 +0200

    Initial commit

---
+++ src/main/java/de/hska/muon/ProductandcategoryserviceApplication.java
@@ -0,0 +1,12 @@
+package de.hska.muon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductandcategoryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductandcategoryserviceApplication.class, args);
+	}
+}
